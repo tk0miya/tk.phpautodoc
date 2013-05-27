a9565236fa533b5bf6b08d49f30712c593e0328b
tk0miya <i.tkomiya@gmail.com>
2013-05-27 19:33:13 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pkg_resources
 
 setup(
      name='tk.phpautodoc',
-     version='0.0.1',
+     version='0.0.2',
      description='sample implementation of phpdocumentor using sphinx',
      author='Takeshi Komiya',
      author_email='i.tkomiya at gmail.com',
